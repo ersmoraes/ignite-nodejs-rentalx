0f34b4aa7b221ae3101e7e7d1a7e9644358331f7
Eric Moraes <ersmoraes@outlook.com>
2022-07-05 14:48:32 -0300

    Criação de APIs, Aplicando o metodo S.O.L.I.D, trabalhando com node streams e documentação via swagger

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+
+const specificationsRoutes = Router();
+
+specificationsRoutes.post("/", (request, response) => {
+  return createCategoryController.handle(request, response);
+});
+
+export { specificationsRoutes };
