d5ff15848b2d55fdbf77671f7de78d5756cc4079
Eric Moraes <ersmoraes@outlook.com>
2022-07-25 13:41:03 -0300

    Criação de token JWT, criptografia de senhas, armazenamento do avatar do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,62 @@
+import "reflect-metadata";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken"
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from "../../../../errors/AppError";
+
+
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const token = sign({}, "5d42160e1f7155c2b1e08764977a37a3", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    }
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase }
